08bac368b54a355f5380d295a3c756761b22b38f
charles <charles@socialfrontier.com>
2024-01-30 13:12:48 +0530

    code updated

--- src/components/dashboard_components/header.jsx
+++ src/components/dashboard_components/header.jsx
@@ -5,7 +5,7 @@ import { getFormatedNo } from '../../utils/Common Functions/common_functions'
 const Header = ({ info }) => {
   return (
     <div className={Style.headerContainer}>
-      <div><h1>{getFormatedNo(info.totalQuizes)}</h1> <h2> Quize Created</h2> </div>
+      <div><h1>{getFormatedNo(info.totalQuizes)}</h1> <h2> Quiz Created</h2> </div>
       <div><h1>{getFormatedNo(info.totalQuestions)}</h1> <h2> Questions Created</h2></div>
       <div><h1>{getFormatedNo(info.totalImpressions)}</h1> <h2> Total Impressions</h2></div>
     </div>
